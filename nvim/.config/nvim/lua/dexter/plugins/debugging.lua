38579fca3bd0475eea048cdd6d00e2d9e5bf7f49
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2024-12-22 16:49:22 +0800

    commit

---
+++ nvim/.config/nvim/lua/dexter/plugins/debugging.lua
@@ -0,0 +1,46 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-telescope/telescope-dap.nvim",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		require("dapui").setup()
+
+		dap.adapters.php = {
+			type = "executable",
+			command = "node",
+			args = { "path/to/vscode-php-debug/out/phpDebug.js" },
+		}
+
+		dap.configurations.php = {
+			{
+				type = "php",
+				request = "launch",
+				name = "Listen for Xdebug",
+				port = 9003,
+			},
+		}
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
+}
