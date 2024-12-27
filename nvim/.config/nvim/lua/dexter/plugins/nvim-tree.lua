f223d310908a56a4431c4703f7db841b667e8de6
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2024-12-27 21:30:41 +0800

    commit

--- nvim/.config/nvim/lua/dexter/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/dexter/plugins/nvim-tree.lua
@@ -1,54 +1,59 @@
-return{
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    -- Recommend settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		-- Recommend settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      -- Change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "+", -- Plus when folder is close
-              arrow_open = "-", -- Minus when folder is open
-            },
-          },
-        },
-      },
-      -- Disable window picker for explorer to work well with window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS.Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			-- Change folder arrow icons
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "+", -- Plus when folder is close
+							arrow_open = "-", -- Minus when folder is open
+						},
+					},
+				},
+			},
+			-- Disable window picker for explorer to work well with window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS.Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
 
-    -- Set Keymaps --
-    local keymap = vim.keymap -- for conciseness
-    
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Open file explorer" })
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Open file explorer on current folder" })
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Close file explorer" })
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
-  end
+		-- Set Keymaps --
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Open file explorer" })
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Open file explorer on current folder" }
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Close file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+	end,
 }
