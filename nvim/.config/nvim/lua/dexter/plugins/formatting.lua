f75b67aff170e54c1678a1d58dfee9ac339c9c48
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-01-30 18:44:52 +0800

    commit

--- nvim/.config/nvim/lua/dexter/plugins/formatting.lua
+++ nvim/.config/nvim/lua/dexter/plugins/formatting.lua
@@ -17,11 +17,11 @@ return {
 				vue = { "prettier" },
 				php = { "pretty-php" },
 			},
-			-- format_on_save = {
-			-- 	lsp_fallback = true,
-			-- 	async = false,
-			-- 	timeout_ms = 1000,
-			-- },
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
