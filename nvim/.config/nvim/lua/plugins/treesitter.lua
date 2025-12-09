3bb15ac895e32855fd30679cbf529ebe40795721
Dexter Alan Balderama <balderamadexteralan@gmail.com>
2025-12-09 22:13:17 +0800

    feat: nvim

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "python"
+      -- add more arguments for adding more treesitter parsers
+    },
+    highlight = {
+    enable = true,
+    },
+  },
+}
